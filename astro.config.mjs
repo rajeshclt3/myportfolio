d4d65122a6d37b98e379055a1ff5dac0ba2879a3
rajeshclt3 <rajesh.tc@teamtumbleweed.eu>
2026-04-16 09:57:08 +0200

    Fixed some configurations

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
+  base: '/myportfolio/',
   integrations: [tailwind(), mdx()],
   site: 'https://rajeshclt3.github.io/myportfolio'
 });
